e6e9dc20a3b970d59a4456d4cc5f67708482d37f
tarosaiba <tarosan.s1117@gmail.com>
2022-02-25 17:04:59 +0900

    fix

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
+# Set up Argo CD
 ## Create Namespace
-kubectl apply -f ./infra
+kubectl apply -f ./infra/argocd-namespace.yaml
 ## Install Argo
 kubectl apply -n argocd -f ./infra/argocd-install.yaml
+## Create Ingress for argocd-server
+kubectl apply -n argocd -f ./infra/argocd-ingress.yaml
+
+# Deploy application by using Argo CD
+kubectl apply -f ./argocd
